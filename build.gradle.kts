464fe976091a77ccf1748cc65c5331959b69ee6b
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2025-08-02 16:36:43 +0530

    chore(deps): update dependency gradle to v9.0.0
    This commit updates Gradle to version 9.0.0.
    It also introduces the use of a version catalog (libs.versions.toml) for managing dependencies and plugins.
    
    Key changes:
    - Updated Gradle wrapper to version 9.0.0.
    - Migrated dependency and plugin declarations to use the version catalog.
    - Updated various library versions, including Kotlin, AndroidX libraries, and other third-party dependencies.
    - Updated `gradle.properties` with new recommended settings and removed outdated ones.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,9 @@
  */
 
 plugins {
-    id("com.android.application") version "8.7.2" apply false
-    id("com.android.library") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
 
 allprojects {
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
+    id(libs.plugins.android.library.get().pluginId)
+    id(libs.plugins.jetbrains.kotlin.android.get().pluginId)
 }
 
 // Libs
-val kotlinVersion = "2.0.21"
 // Configs
 val major = 0
 val minor = 6
@@ -18,11 +17,10 @@ kotlin {
 android {
     namespace = "com.afterroot.utils"
 
-    compileSdk = 35
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         minSdk = 23
-        resourceConfigurations.addAll(listOf("en"))
     }
 
     buildTypes {
@@ -36,19 +34,21 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    println(getLayout().buildDirectory.get().asFile.path)
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation(libs.kotlin.stdLib)
 
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.6")
-    implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("androidx.transition:transition:1.5.1")
-    implementation("com.afollestad.material-dialogs:core:3.3.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("com.google.code.gson:gson:2.12.1")
-    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation(libs.androidx.appCompat)
+    implementation(libs.androidx.constraintLayout)
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.fragment)
+    implementation(libs.androidx.preference)
+    implementation(libs.androidx.transition)
+    implementation(libs.afollestad.materialdialogs.core)
+    implementation(libs.google.material)
+    implementation(libs.google.gson)
+    implementation(libs.timber)
 }
